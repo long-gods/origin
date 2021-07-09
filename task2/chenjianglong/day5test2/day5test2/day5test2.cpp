1ca57f4949fce2891397306eb3478ff7860294fa
chenjianglong <980464102@qq.com>
2021-07-09 23:58:23 +0800

    new

---
+++ task2/chenjianglong/day5test2/day5test2/day5test2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream> 
+using namespace std;
+class MyInt
+{
+	int nVal;
+public:
+	MyInt(int n) { nVal = n; }
+	MyInt operator-(int i);// 在此处补充你的代码
+	friend int Inc(MyInt MY);
+};
+int Inc(MyInt MY) {
+	return MY.nVal + 1;
+}
+int main() {
+	int n;
+	while (cin >> n) {
+		MyInt objInt(n);
+		MyInt o=objInt - 2 - 1 - 3;
+		cout << Inc(o);
+		cout << ",";
+		MyInt ob=o - 2 - 1;
+		cout << Inc(ob) << endl;
+		//MyInt objInt(n);
+		//objInt - 2 - 1 - 3;
+		//cout << Inc(objInt);
+		//cout << ",";
+		//objInt - 2 - 1;
+		//cout << Inc(objInt) << endl;
+	}
+	return 0;
+}
+
+MyInt MyInt::operator-(int i)
+{
+	nVal -= i;
+	return *this;// TODO: 在此处插入 return 语句
+}
