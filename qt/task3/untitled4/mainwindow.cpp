134ee04bd418ee4a8d5becd1bc4084150923814a
chenjianglong <980464102@qq.com>
2021-07-22 01:02:44 +0800

    1

---
+++ qt/task3/untitled4/mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<qfilesystemwatcher.h>
+#include<QFile>
+#include<QtDebug>
+
+QFileSystemWatcher  m_watch;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+   qDebug()<< m_watch.addPath("C:\\Users\\cjl\\source\\repos\\qt\\task3\\log.txt");
+     connect(&m_watch, SIGNAL(fileChanged(QString)), this, SLOT(fileUpdated()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::fileUpdated()
+{
+    QFile file("C:\\Users\\cjl\\source\\repos\\qt\\task3\\log.txt");
+    qDebug()<<file.open(QIODevice::ReadOnly);
+    QString str=file.readAll();
+    qDebug()<<str;
+    ui->textEdit->setText(str);
+    file.close();
+}
